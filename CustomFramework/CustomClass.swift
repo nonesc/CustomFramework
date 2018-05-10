dbf22e7690ae3396f8b5fed3fa5f2927448edb83
sujin <sujinchaichanamongkol@gmail.com>
2018-05-10 15:29:05 +0700

    commit

---
+++ CustomFramework/CustomClass.swift
@@ -0,0 +1,20 @@
+//
+//  CustomClass.swift
+//  CustomFramework
+//
+//  Created by Sujin chaichanamongkol on 10/5/2561 BE.
+//  Copyright © 2561 Sujin chaichanamongkol. All rights reserved.
+//
+
+import Foundation
+
+public class CustomClass {
+    
+    public init() {
+        
+    }
+    
+    public func doSomething() {
+        print("gg")
+    }
+}
---
+++ CustomFramework/CustomFramework.h
@@ -0,0 +1,19 @@
+//
+//  CustomFramework.h
+//  CustomFramework
+//
+//  Created by Sujin chaichanamongkol on 10/5/2561 BE.
+//  Copyright © 2561 Sujin chaichanamongkol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomFramework.
+FOUNDATION_EXPORT double CustomFrameworkVersionNumber;
+
+//! Project version string for CustomFramework.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFramework/PublicHeader.h>
+
+
